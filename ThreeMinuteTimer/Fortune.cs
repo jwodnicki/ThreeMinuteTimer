c48f0ceb02abe035165e638bf21d5d60c0f3adb0
jwodnicki <jwodnicki@gmail.com>
2013-04-04 16:18:26 -0700

    three minute timer

---
+++ ThreeMinuteTimer/Fortune.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ThreeMinuteTimer
+{
+	class Fortune
+	{
+		private List<string> fortunes;
+
+		public Fortune()
+		{
+			string s = "";
+			string t;
+			fortunes = new List<string>();
+			Assembly ass = this.GetType().Assembly;
+			using (Stream file = ass.GetManifestResourceStream("ThreeMinuteTimer.Resources.platitudes"))
+			{
+				using (TextReader reader = new StreamReader(file))
+				{
+					while ((t = reader.ReadLine()) != null)
+					{
+						if (t == "%")
+						{
+							fortunes.Add(s);
+							s = "";
+						}
+						else
+						{
+							s += t;
+						}
+					}
+				}
+			}
+		}
+
+		public string GetRandom()
+		{
+			return fortunes[new Random().Next(fortunes.Count)];
+		}
+	}
+}
---
+++ ThreeMinuteTimer/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Shell;
+
+namespace ThreeMinuteTimer
+{
+	public partial class MainWindow : Window
+	{
+		private Fortune cookie;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			WindowState = WindowState.Minimized;
+		}
+
+		private void eventLoad(object sender, RoutedEventArgs e)
+		{
+			amrac();
+			cookie = new Fortune();
+		}
+
+		private void amrac()
+		{
+			int decisecondsToRun = 1800;
+
+			TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Normal;
+			int deciSecondsRemaining = decisecondsToRun;
+			var andre = new BackgroundWorker();
+			andre.WorkerReportsProgress = true;
+			andre.ProgressChanged += (sender, e) => TaskbarItemInfo.ProgressValue = (double)e.ProgressPercentage / 100;
+			andre.DoWork += (sender, e) =>
+			{
+				while (deciSecondsRemaining-- > 0)
+				{
+					Application.Current.Dispatcher.Invoke(() => textBlock.Text = "" + deciSecondsRemaining / 10);
+					Thread.Sleep(100);
+					int progressPercent = 100 * deciSecondsRemaining / decisecondsToRun;
+					andre.ReportProgress(progressPercent);
+					if (progressPercent < 33)
+					{
+						Application.Current.Dispatcher.Invoke(() => TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Error);
+					}
+					else if (progressPercent < 67)
+					{
+						Application.Current.Dispatcher.Invoke(() => TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Paused);
+					}
+				}
+			};
+			andre.RunWorkerCompleted += (sender, e) =>
+			{
+				MessageBox.Show(cookie.GetRandom());
+				Application.Current.Shutdown();
+			};
+			andre.RunWorkerAsync();
+		}
+	}
+}
